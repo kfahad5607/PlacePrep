31037a46e44c51be34103831741a28d37900a931
shaibaz <shaibazansarii.48@gmail.com>
2021-01-25 17:47:53 +0530

    Added aptiQuestion Model

---
+++ app.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const routes = require('./onlineJudge/index');
+const path = require('path');
+const { exec, spawn } = require('child_process');
+
+const app = express();
+
+// Parsing the data into body
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (req, res) => {
+
+    res.json({
+        data: 'hello'
+
+    });
+});
+
+app.use('/api', routes);
+
+app.get('/run1', (req, res) => {
+    exec('g++ ./main1.cpp -o main', (err, stdout, stderr) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('output', stdout);
+        }
+    });
+
+    res.json({
+        data: 'running...'
+    });
+});
+
+
+app.get('/run2', (req, res) => {
+
+    exec('.\\main.exe < ./onlineJudge/input.txt ', (err, stdout, stderr) => {
+        if (err) {
+            console.log(err);
+        } else {
+            console.log('output', stdout);
+        }
+    });
+
+    res.json({
+        data: 'running...'
+    });
+});
+
+
+
+module.exports = app;
---
+++ models/aptiQuestionModel.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const aptiQuestionSchema = new mongoose.Schema({
+    question: {
+        type: String,
+        required: [true, "A Question must have a question"],
+        unique: true,
+        trim: true
+    },
+    // answers: [String],
+    answers: {
+        type: [String],
+        required: [true, "Each question must have options"]
+    },
+    correctAnswer: {
+        type: String,
+        required: [true, "Each question must have correct answer"]
+    },
+    topic: {
+        type: String,
+        required: [true, "Each question must belong to particular topic"]
+    },
+    category: {
+        type: String,
+        // default: 'Quants',
+        enum: {
+            values: ['Quants', 'Logical','Verbal', 'other'],
+            message: 'category is either: Quants, Logical or Verbal'
+        }
+    },
+    explanation: {
+        type: String,
+        default: 'No answer description available for this question'
+    }
+})
+
+// company type questions?
+const AptiQuestion = mongoose.model('aptiQuestion', aptiQuestionSchema)
+module.exports = AptiQuestion
