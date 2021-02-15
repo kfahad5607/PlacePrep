8d91a118ad55d1d57eceb222d3db8156782f1bb1
shaibaz <shaibazansarii.48@gmail.com>
2021-02-15 16:51:08 +0530

    Added GetUser and logout

--- controllers/authController.js
+++ controllers/authController.js
@@ -66,6 +66,15 @@ exports.login = catchAsync(async (req, res, next) => {
     createSendToken(user, 200, res);
 });
 
+exports.logout = (req, res) => {
+    res.cookie('jwt', 'LoggedOut', {
+        expires: new Date(Date.now() + 0 * 1000),
+        httpOnly: true
+    })
+    res.status(200).json({
+        status: 'success'
+    })
+}
 
 
 exports.protect = catchAsync(async (req, res, next) => {
@@ -74,9 +83,9 @@ exports.protect = catchAsync(async (req, res, next) => {
     if (req.headers.authorization && req.headers.authorization.startsWith('Bearer')) {
         token = req.headers.authorization.split(' ')[1];
     }
-    // else if (req.cookies.jwt) {
-    //     token = req.cookies.jwt;
-    // }
+    else if (req.cookies.jwt) {
+        token = req.cookies.jwt;
+    }
 
     if (!token) {
         return next(
